3f6a7586c25bf4e7e90ad182aaa3827638dc1a7c
guimonz <guilherme@konkix.com>
2015-03-02 11:30:25 -0300

    adding create city feature

--- public/site.js
+++ public/site.js
@@ -12280,10 +12280,10 @@ var jQuery = require('jquery'),
     $ = jQuery;
 
 /* ========================================================================
- * Bootstrap: tab.js v3.3.1
+ * Bootstrap: tab.js v3.3.2
  * http://getbootstrap.com/javascript/#tabs
  * ========================================================================
- * Copyright 2011-2014 Twitter, Inc.
+ * Copyright 2011-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
 
@@ -12298,7 +12298,7 @@ var jQuery = require('jquery'),
     this.element = $(element)
   }
 
-  Tab.VERSION = '3.3.1'
+  Tab.VERSION = '3.3.2'
 
   Tab.TRANSITION_DURATION = 150
 
